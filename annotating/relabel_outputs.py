cf77cb779c028db8381b48b9eae31165e9fb2122
DMahamedi <dmaha004@ucr.edu>
2024-10-07 11:02:38 -0700

    Add module documentation to relabel_outputs

--- annotating/relabel_outputs.py
+++ annotating/relabel_outputs.py
@@ -1,6 +1,14 @@
 import os
 
 def rewrite_annotation_label(annotation_filepath: str, new_class_id: int) -> None:
+    '''
+        Relabel the model-assigned class id to a custom class id for a specific annotation
+        :params:
+            annotation_filepath: str path to a specific annotation .txt file
+            new_class_id: new class id to give the annotation
+        :returns:
+            None
+    '''
     with open(annotation_filepath, 'r+') as f:
         line = f.readline()
         values = line.strip().split()
@@ -9,6 +17,13 @@ def rewrite_annotation_label(annotation_filepath: str, new_class_id: int) -> Non
         f.write(' '.join(values))
 
 def rewrite_annotation_class(class_name: str, new_class_id: int) -> None:
+    '''
+        Relabel all of a new class types annotations to have a unique class id
+        Needed for converting generic classes (i.e. person) to something unique (like a specific individual)
+        :params:
+            class_name: name of the new class we want to label
+            new_class_id: the class id we want to give the new class
+    '''
     annotation_folder_path = f'./Data/{class_name}/Labels/'
     
     for annotation_file in os.listdir(annotation_folder_path):
