088874ccaa357b4a283e429e22bf70d7df3903ac
DMahamedi <dmaha004@ucr.edu>
2024-10-07 12:28:49 -0700

    incorporate config changes to main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from image_collection.get_images_with_oak import collect_data
 from image_collection.load_from_video import get_images_from_video
 from annotating.annotate_data import annotate_images
 from annotating.review_annotations import filter_annotations
+from annotating.relabel_outputs import rewrite_annotation_class
 
 
 def main():
+
     if config.CREATE_IMAGES and not config.USE_EXISTING_VIDEO:
         collect_data(
             config.NEW_CLASS_NAME, config.CLASS_TYPE, config.CLASS_ID,
@@ -14,13 +16,16 @@ def main():
     elif config.CREATE_IMAGES:
         get_images_from_video(config.VIDEO_PATH, config.NEW_CLASS_NAME, config.CLASS_ID)
 
-    if config.ANNOTATE_IMAGES and not config.NEW_CLASS_ID:
+    if config.ANNOTATE_IMAGES and not config.ANNOTATE_NEW_CLASS_ID:
         annotate_images(config.NEW_CLASS_NAME, config.YOLO_MODEL, config.CLASS_ID)
-    elif config.ANNOTATE_IMAGES:
+    elif config.ANNOTATE_NEW_CLASS_ID:
         annotate_images(config.NEW_CLASS_NAME, config.YOLO_MODEL, config.CLASS_ID, config.NEW_CLASS_ID)
     
     if config.FILTER_ANNOTATIONS:
         filter_annotations(config.CLASS_ID, config.NEW_CLASS_NAME)
+
+    if config.RELABEL_ANNOTATION_CLASS_ID:
+        rewrite_annotation_class(config.NEW_CLASS_NAME, config.NEW_CLASS_ID)
 
 if __name__ == "__main__":
     main()
